3cbe5aefa8966a8e2a402d012ea89e0e92896c41
Ruben Menino <ruben.menino@outlook.com>
2021-01-07 21:44:40 +0000

    contorno, nao sei se é suposto só detetar a carta() deteta tudo) o thresh tb esta um bocado mamadoo

--- cardRecognize.py
+++ cardRecognize.py
@@ -1,12 +1,12 @@
 from cv2 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(2)
 
 def imageProcessing(img): 
     n = 5
     sygmaY = 100
-    thresh = 200 #  200 de thresh
+    thresh = 70 #  200 de thresh
 
     # to gray scale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -24,6 +24,18 @@ while True:
 
     ## processar a imagem
     modified = imageProcessing(frame)
+    mode = cv2.RETR_EXTERNAL
+    method = cv2.CHAIN_APPROX_SIMPLE
+    cardsContours = []
+    ## desenhar o contorno (encontra vários contornos)
+    contours, hierarchy = cv2.findContours(modified, mode, method) 
+    contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]  # até 10
+
+    for con in contours:         
+        cv2.drawContours(frame, contours, -1, (0, 0, 255), 3) # -1, tudo 
+
+    
+    
 
     cv2.imshow('frame', frame)
     cv2.imshow('modified', modified)
