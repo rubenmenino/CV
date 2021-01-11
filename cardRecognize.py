4974c402333919e0f9cadaebe4f80dd85c1551e8
Ruben Menino <ruben.menino@outlook.com>
2021-01-11 18:19:58 +0000

    ajuste de thresh para a camara, e n pontos da carta

--- cardRecognize.py
+++ cardRecognize.py
@@ -6,7 +6,7 @@ cap = cv2.VideoCapture(2)
 def imageProcessing(img): 
     n = 5
     sygmaY = 100
-    thresh = 70 #  200 de thresh
+    thresh = 130 #  200 de thresh
 
     # to gray scale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -29,13 +29,16 @@ while True:
     cardsContours = []
     ## desenhar o contorno (encontra vários contornos)
     contours, hierarchy = cv2.findContours(modified, mode, method) 
-    contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]  # até 10
-
-    for con in contours:         
-        cv2.drawContours(frame, contours, -1, (0, 0, 255), 3) # -1, tudo 
-
     
-    
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        approx = cv2.approxPolyDP(cnt, 0.015* cv2.arcLength(cnt, closed=True), closed=True)
+
+
+        if len(approx) == 4: #numero de pontos (retangulo = 4)
+            cv2.drawContours(frame, contours, -1, (0, 0, 255), 3) # -1, tudo 
+            
+
 
     cv2.imshow('frame', frame)
     cv2.imshow('modified', modified)
