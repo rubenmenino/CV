619501cd39fa34fa93044d9a64acccff5374e841
Ruben Menino <ruben.menino@outlook.com>
2021-01-12 01:11:13 +0000

    retangulos em carta e text do numero de cartas

--- cardRecognize.py
+++ cardRecognize.py
@@ -3,43 +3,67 @@ import numpy as np
 
 cap = cv2.VideoCapture(2)
 
+def nothing(x):
+    pass
+
+
+def numberOfCards(image):
+    font = cv2.FONT_HERSHEY_SIMPLEX 
+    # org 
+    org = (50, 50) 
+    # fontScale 
+    fontScale = 1
+    # Blue color in BGR 
+    color = (255, 0, 0) 
+    # Line thickness of 2 px 
+    thickness = 2
+    # Using cv2.putText() method 
+    image = cv2.putText(frame, numContoursStr, org, font,  
+                   fontScale, color, thickness, cv2.LINE_AA) 
+
 def imageProcessing(img): 
     n = 5
     sygmaY = 100
-    thresh = 130 #  200 de thresh
-
+    thresh = 130 #  200 de thresh     # para o meu setup
     # to gray scale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
     # por a imagem mais baça
     smooth = cv2.GaussianBlur(gray, (n, n), sygmaY)
-
     # o valor para a trash está para um fundo preto(200)
-    val, threshImage = cv2.threshold(smooth, thresh, 255, cv2.THRESH_BINARY)
+    val, threshImage = cv2.threshold(smooth, t, 255, cv2.THRESH_BINARY)
     return threshImage
 
 
 while True:
     ret, frame = cap.read()
-
+    t = cv2.getTrackbarPos('t', 'modified') # ver depois
     ## processar a imagem
     modified = imageProcessing(frame)
     mode = cv2.RETR_EXTERNAL
     method = cv2.CHAIN_APPROX_SIMPLE
-    cardsContours = []
+    cards = []
     ## desenhar o contorno (encontra vários contornos)
     contours, hierarchy = cv2.findContours(modified, mode, method) 
-    
-    for cnt in contours:
-        area = cv2.contourArea(cnt)
-        approx = cv2.approxPolyDP(cnt, 0.015* cv2.arcLength(cnt, closed=True), closed=True)
 
+    numContours = len(contours) - 2 # dáme sempre dois a mais, nao faço a minima porque, (-2)
+    numContoursStr = 'Existem ' + str(numContours) + ' cartas'
 
-        if len(approx) == 4: #numero de pontos (retangulo = 4)
-            cv2.drawContours(frame, contours, -1, (0, 0, 255), 3) # -1, tudo 
+    ## chamar a função texto numero de cartas 
+    numberOfCards(frame)
+
+    #print(len(contours) - 2)
+    for con in contours:
+        area = cv2.contourArea(con)
+        approx = cv2.approxPolyDP(con, 0.015*cv2.arcLength(con, closed=True) , closed=True)
+
+        #print("contours", cnt)
+        if len(approx) == 4 and area > 20000: #numero de pontos (retangulo = 4)
+            x, y, w, h = cv2.boundingRect(approx)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 2)
+            cards.append(con)
             
 
-
+    cv2.createTrackbar("t", "modified", 130, 200, nothing)   ## queria mudar em tempo real, idk como fazer
     cv2.imshow('frame', frame)
     cv2.imshow('modified', modified)
 
