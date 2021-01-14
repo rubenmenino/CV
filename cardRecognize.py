1d87202c0f004bd6dc0fe60b68b766786ee83de5
Ruben Menino <ruben.menino@outlook.com>
2021-01-14 18:13:36 +0000

    faz a perspectiveTransform (hotizontal), e order Points está só para uma carta

--- cardRecognize.py
+++ cardRecognize.py
@@ -7,6 +7,22 @@ def nothing(x):
     pass
 
 
+def ordenatePoints(points): #bottom right, #bottom left,  # top left,  # top right,     # retorna os primeiros 4 pontos (funciona se tiver uma carta)
+    arr = np.array(points)[:, 0] # Remove singleton dimension
+    # Find the centroid
+    cen = np.mean(arr, axis=0)
+    # Find the angle from each point to its centroid
+    angles = np.arctan2(arr[:,1] - cen[1], arr[:,0] - cen[0])
+    # Because arctan spans -pi to pi, let's switch to 0 to 2*pi
+    angles[angles < 0] = angles[angles < 0] + 2*np.pi
+    # Order based on angle - ascending order
+    ind = np.argsort(angles)
+    # Reorder your points
+    arr_sorted = arr[ind] 
+
+    return arr_sorted
+
+
 def numberOfCards(image):
     font = cv2.FONT_HERSHEY_SIMPLEX 
     # org 
@@ -38,13 +54,12 @@ while True:
     ret, frame = cap.read()
     t = cv2.getTrackbarPos('t', 'modified') # ver depois
     ## processar a imagem
+    img2 = frame.copy()
     modified = imageProcessing(frame)
     mode = cv2.RETR_EXTERNAL
     method = cv2.CHAIN_APPROX_SIMPLE
-    cards = []
     ## desenhar o contorno (encontra vários contornos)
     contours, hierarchy = cv2.findContours(modified, mode, method) 
-    countMoreNumber = []
     pointsCards = []
     #print(len(contours))
     numContours = len(contours) - 2 # dáme sempre dois a mais, nao faço a minima porque, (-2)
@@ -57,7 +72,7 @@ while True:
 
     ## chamar a função texto numero de cartas 
     numberOfCards(frame)
-    four_corners_set = []
+    fourPointsCard = []
     #print(len(contours) - 2)
     for con in contours:
         area = cv2.contourArea(con)
@@ -80,6 +95,7 @@ while True:
                 secondPoint = points[1]
                 thirdPoint = points[2]
                 fourPoint = points[3]
+                
             
 
             #print("firstPoint", firstPoint)      
@@ -87,11 +103,32 @@ while True:
             #print("thirdPoint", thirdPoint)      
             #print("fourPoint", fourPoint)        
 
+            fourPointsCard.append(firstPoint)
+            fourPointsCard.append(secondPoint)
+            fourPointsCard.append(thirdPoint)
+            fourPointsCard.append(fourPoint)
+            
+            
+            #print(fourPointsCard)
+            a = ordenatePoints(fourPointsCard)
+            
+            # perspective transform (https://www.geeksforgeeks.org/perspective-transformation-python-opencv/) # função opencv
+            src = np.float32(a)
+            #print(src)
+            # now define which corner we are referring to
+            dst = np.float32([[0, 0], [0, 400], [300, 400], [300, 0]])  # top left, # top right, #bottom left, #bottom right
+
+            # transformation matrix
+            matrix = cv2.getPerspectiveTransform(src, dst)
+
+            resultPerspective = cv2.warpPerspective(frame, matrix, (300, 400))
 
 
-    cv2.createTrackbar("t", "modified", 130, 200, nothing)   ## queria mudar em tempo real, idk como fazer
+    cv2.createTrackbar("t", "modified", 125, 200, nothing)   ## queria mudar em tempo real, idk como fazer
     cv2.imshow('frame', frame)
     cv2.imshow('modified', modified)
+    cv2.imshow('wrap', resultPerspective)
+
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
